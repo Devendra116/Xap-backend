04b78ea20919351acc0f3318967c7ae69f3b165e
Devendra116 <chauhandevendra116@gmail.com>
2024-07-09 22:12:44 +0530

    add cors in server

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db')
+const cors = require('cors');
 
 const app = express()
 app.use(express.json());
 
+app.use(cors());
 app.get('/', (req, res) => {
     res.json({
         message: "Welcome to Xap"
